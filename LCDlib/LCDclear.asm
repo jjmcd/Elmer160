616cb46dd692ce934a4126adfebeafbff4e3d767
John J. McDonough <jjmcd@fedoraproject.org>
2004-09-26 15:18:02 -0400

    Use LCDmacs instead of Picklemacs

--- LCDlib/LCDclear.asm
+++ LCDlib/LCDclear.asm
@@ -1,18 +1,18 @@
-		include		"PickleMacs.inc"
+		include		"LCDMacs.inc"
 
 	; Provided Routines
 		global		LCDclear
 	; Required routines
-		extern		SendI
-		extern		waste
-		extern		wastel
+		extern		LCDsndI
+		extern		Del40us
+		extern		Del2ms
 
 		code
 ; ------------------------------------------------------------------------
 	; Clear the LCD
 LCDclear:
 		LCD16	H'00',H'01'
-		call	wastel
+		call	Del2ms
 		return
 
 		end
