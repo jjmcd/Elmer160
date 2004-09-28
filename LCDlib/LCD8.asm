55b848b73ee59f0b9052cb5fe770b72682d756e6
John J. McDonough <jjmcd@fedoraproject.org>
2004-09-28 16:10:38 -0400

    Use LCDmacs instead of PickleMacs

--- LCDlib/LCD8.asm
+++ LCDlib/LCD8.asm
@@ -1,3 +1,27 @@
+		title		'LCD8 - Set the LCD DDRAM address to 8'
+		subtitle	'Part of the LCDlib library'
+		list		b=4,c=132,n=77,x=Off
+
+;**
+;  LCD8
+;
+;  Set the LCD DDRAM address to 8.
+;
+;  This function sets the DDRAM address of the LCD
+;  to 8.  The DDRAM address determines where in the LCD
+;  memory the next character will be displayed.
+;
+;  Setting the DDRAM address to 8 causes the next
+;  character to appear at the right-hand side of
+;  an 8 character display.  This is useful for
+;  scrolling characters across the display from
+;  right to left.
+;
+;  The contents of the W register are ignored by
+;  this routine.  The contents of the W register
+;  are destroyed on exit.
+;
+;**
 		include		"LCDMacs.inc"
 
 	; Provided Routines
@@ -5,7 +29,7 @@
 	; Required routines
 		extern		LCDsndI		; Send a command bybble to the LCD
 		extern		Del40us		; Delay 40 usec
-		extern		Del2ms		; Delay 1.7 msec
+		extern		Del2ms		; Delay 1.8 msec
 
 		code
 ; ------------------------------------------------------------------------
