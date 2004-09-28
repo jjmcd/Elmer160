522db45d7df7abf61cb9381ed86f9139b57c7d87
John J. McDonough <jjmcd@fedoraproject.org>
2004-09-28 09:19:06 -0400

    Attempt to document for doxygen

--- LCDlib/LCDzero.asm
+++ LCDlib/LCDzero.asm
@@ -1,3 +1,26 @@
+#ifdef never
+/*! \file LCDzero.asm
+
+ \brief Set the LCD DDRAM address to zero.
+
+ Provides the function LCDzero.  Requires the functions
+ LCDsndI, Del40us and Del2ms.
+
+*/
+
+/*! \function LCDzero.asm
+
+ \brief Set the LCD DDRAM address to zero
+
+
+ LCDzero is called when it is desired to begin a new display on the
+ LCD.  The DDRAM address, which affects where the next character will
+ be written, is set to zero which is the first position on the LCD.
+
+ The W register is not preserved and is ignored.
+*/
+#endif
+
 		include		"LCDMacs.inc"
 
 	; Provided Routines
