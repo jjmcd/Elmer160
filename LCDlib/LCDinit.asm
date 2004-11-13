c6007d770fcc337ee1b1031b26467e21201eac95
John J. McDonough <jjmcd@fedoraproject.org>
2004-11-13 16:18:00 -0500

    Add code for 16F88

--- LCDlib/LCDinit.asm
+++ LCDlib/LCDinit.asm
@@ -40,13 +40,27 @@ _LCDV01	res			1			; Storage for loop counter
 LCDinit:
 		; Initialize the LCD to 4 bits
 		;
+		; Set the ports in case the user has forgotten
+		movlw		H'80'		; Turn off low 7 bits
+		IFDEF		__16F88		; For 16F88 only
+		andwf		PORTA,F		; of PORTA
+		ENDIF
+		errorlevel	-302		; Suppress message
+		banksel		TRISB		; Now set the low 7 bits of
+		andwf		TRISB,F		; PORTB to outputs
+		IFDEF		__16F88		; For 16F88 only
+		banksel		ANSEL		; Need to set the A/D converter
+		andwf		ANSEL,F		; pins to normal
+		ENDIF
+		banksel		PORTB		; Backto bank zero and
+		errorlevel	+302		; re-enable the error message
+
 		; First, need to wait a long time after power up to
 		; allow time for 44780 to get it's act together
 		movlw		020h		; Need >15.1ms after 4.5V
 		movwf		_LCDV01		; we will wait 65ms after 2V
-
-		call		Del2ms		;
-		decfsz		_LCDV01,F
+		call		Del2ms		; (in the case of LF parts)
+		decfsz		_LCDV01,F	;
 		goto		$-2
 
 		; Initialization begins with sending 0x03 3 times followed
@@ -55,9 +69,13 @@ LCDinit:
 		LCD16		H'03',h'02'
 
 		; Now set up the display the way we want it
-		LCD16		H'02',H'08'	; Set lines, font
+		IFDEF		LCD2LINE
+		LCD16		H'02',H'0c'	; Set lines, font
 						; 7, 1=2 lines; 6, 0=5x7; 5, 4, don't care 
 						;;; Note 0c0, 080 seems no diff on DMC20434
+		ELSE
+		LCD16		H'02',H'00'	; Set lines, font
+		ENDIF
 		LCD16		H'00',H'08'	; Display Off
 		LCD16		H'00',H'01'	; Display On
 		LCD16		H'00',H'06'	; Set Entry Mode
