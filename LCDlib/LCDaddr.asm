c1cb8b3b993bbdeb9d22b4906ca61a409cc7101c
John J. McDonough <jjmcd@fedoraproject.org>
2004-11-13 15:12:00 -0500

    Initial revision

---
+++ LCDlib/LCDaddr.asm
@@ -0,0 +1,50 @@
+		title		'LCDaddr - Set the LCD display address'
+		subtitle	'Part of the LCDlib library'
+		list		b=4,c=132,n=77,x=Off
+
+;**
+;  LCDaddr
+;
+;  Set the LCD display address.
+;
+;  This function sets the display address for the next character
+;  to be displayed on the LCD.  The caller passes in the address
+;  in the W register.
+;
+;  For the PIC-EL 16 character display, the ninth character is
+;  at address 32 (decimal).  For most other 2 line displays, the
+;  first character of the second line is at address 64.
+;
+;  The contents of the W register are destroyed.
+;
+;**
+		include		"LCDMacs.inc"
+
+	; Provided Routines
+		global		LCDaddr
+	; Required routines
+		extern		LCDsndI
+		extern		Del40us
+		extern		Del2ms
+
+; ------------------------------------------------------------------------
+	; Set the LCD DDRAM address
+_LCDOV1	udata_ovr
+Addr	res			1
+
+		code
+LCDaddr:
+		movwf		Addr		; Save off address
+		swapf		Addr,W		; Will send high byte first
+		andlw		h'0f'		; Mask unneeded bits
+		iorlw		h'08'		; Set command bit on
+		call		LCDsndI		; Send high byte to LCD
+		call		Del40us		; 40us
+
+		movf		Addr,W		; Grab the mow byte
+		andlw		H'0f'		; Mask off high
+		call		LCDsndI		; Send to LCD
+		call		Del2ms		; 4.1ms
+		return
+
+		end
---
+++ LCDlib/LCDmsg.asm
@@ -0,0 +1,75 @@
+		title		'LCDmsg - Display message on LCD'
+		subtitle	'Part of the LCDlib library'
+		list		b=4,c=132,n=77,x=Off
+
+;**
+;  LCDmsg
+;
+;  Display a message on the LCD.
+;
+;  This function displays a message stored in the general
+;  purpose registers on the LCD.  The caller prepares a
+;  buffer in the GPRs containing the length of the message
+;  followed by the message text.  The caller then calls
+;  LCDmsg with the address of that buffer in the W register.
+;
+;  The contents of the W register are destroyed.
+;**
+		include		LCDmacs.inc
+
+		global		LCDmsg
+		extern		LCDletr,LCDaddr
+
+_LCDOV2	udata_ovr
+BufAddr	res			1
+Count	res			1
+Switch	res			1
+
+		code
+; ------------------------------------------------------------------------
+;**
+;  Display a message on the LCD.
+;
+;  LCDmsg sends a message to the LCD.  Enter with
+;  the file register address of a message buffer.
+;  The buffer contains the message length, 1-16,
+;  followed by the message.
+;
+;  If the message is greater than 8 characters,
+;  LCDmsg displays the remainder of the message
+;  (chars 9-16) on the second line, presuming the
+;  PIC-EL 8 char by 2 line display.
+;
+;**
+LCDmsg
+		movwf		BufAddr		; Save off the buffer address
+		movwf		FSR			; and write into FSR
+		movf		INDF,W		; so we can pick up the length
+		movwf		Count		; and save it
+		IFDEF		LCD2LINE
+		incf		BufAddr,F	; Point to the beginning of text
+		movf		BufAddr,W	; And calculate where the start
+		addlw		.8			; of line 2 is on the 16
+		movwf		Switch		; character display
+		ENDIF
+
+MsL
+		IFDEF		LCD2LINE
+		movf		Switch,W	; Pick up the change line address
+		xorwf		BufAddr,W	; Are we there yet?
+		btfss		STATUS,Z
+		goto		MsL1		; No, continue on
+		movlw		.32			; Yes, point to the start of 
+		call		LCDaddr		; line 2 in the LCD memory
+MsL1
+		ENDIF
+		movf		BufAddr,W	; Move the address of the next
+		movwf		FSR			; letter into FSR so we can
+		movf		INDF,W		; pick up the letter
+		call		LCDletr		; Display the character
+		incf		BufAddr,F	; Point to next char in message
+		decfsz		Count,F		; Count down characters
+		goto		MsL			; Not done, go do next char
+
+		return
+		end
