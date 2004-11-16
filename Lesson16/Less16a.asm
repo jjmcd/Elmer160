7a3b7ec75d338dcf97e927dee984afa951cc59af
John J. McDonough <jjmcd@fedoraproject.org>
2004-11-16 10:04:28 -0500

    Additional comments

--- Lesson16/Less16a.asm
+++ Lesson16/Less16a.asm
@@ -1,20 +1,34 @@
+; Less16a.asm
+;
+;	Simple mainline to demonstrate linking.  Simply
+;	calls a subroutine and twiddles a GPR location.
+;
+;	WB8RCR - 29-Oct-04
+;	$Revision: 1.3 $ $Date: 2004-11-16 10:04:28-05 $
+;===========================================================
+
 		processor	pic16f84a
 		include		p16f84a.inc
 		__config	_XT_OSC & _WDT_OFF & _PWRTE_ON
 
+;	Need to define the external entry point we require so that
+;	the assembler doesn't complain it's undefined
 		extern		aSub
 
+;	Section of data in GP registers
 		udata
 var1	res			1
 
+;	Startup code located where the PIC goes at reset
 STARTUP	code
 		goto		Start
 
+;	Main part ofthe code wherever the linker cares to put it
 		code
 Start
-		movlw		h'17'
-		call		aSub
-		movwf		var1
+		movlw		h'17'	; Put something in the W register
+		call		aSub	; Go to work on it
+		movwf		var1	; Save off the result
 
 aa		goto		aa
 		end
--- Lesson16/Less16a1.asm
+++ Lesson16/Less16a1.asm
@@ -1,11 +1,21 @@
+; Less16a1.asm
+;
+;	Simple subroutine to demonstrate linking.  Merely
+;	complements the contents of the W register
+;
+;	WB8RCR - 29-Oct-04
+;	$Revision: 1.3 $ $Date: 2004-11-16 10:02:52-05 $
+;===========================================================
+
 		processor	pic16f84a
 		include		p16f84a.inc
 
+;	Define the global entry point so the linker can see it
 		global		aSub
 
 		code
 aSub
-		xorlw		h'ff'
-		return
+		xorlw		h'ff'	; Only complement the contents
+		return				; of W and return
 
 		end
