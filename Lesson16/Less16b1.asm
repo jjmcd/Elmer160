5cd0fe636053c1d6d5cb5b3dc0692b38d4b4c67e
John J. McDonough <jjmcd@fedoraproject.org>
2004-11-16 11:30:54 -0500

    Correct spelling in comment

--- Lesson16/Less16b1.asm
+++ Lesson16/Less16b1.asm
@@ -1,10 +1,10 @@
 ; Less16b1.asm
 ;
 ;	Simple subroutine to demonstrate linking.  Provide
-;	Storage for a variable to be used by another routine.
+;	storage for a variable to be used by another routine.
 ;
 ;	WB8RCR - 16-Nov-04
-;	$Revision: 1.1 $ $Date: 2004-11-16 11:24:20-05 $
+;	$Revision: 1.2 $ $Date: 2004-11-16 11:30:54-05 $
 ;===========================================================
 
 		global		Sub1
