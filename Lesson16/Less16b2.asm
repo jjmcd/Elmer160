ff120f2080708aca4a54c410ec4862ff1e9f565f
John J. McDonough <jjmcd@fedoraproject.org>
2005-01-20 16:23:36 -0500

    Correct filename in comment

--- Lesson16/Less16b2.asm
+++ Lesson16/Less16b2.asm
@@ -1,10 +1,10 @@
-; Less16b1.asm
+; Less16b2.asm
 ;
 ;	Simple subroutine to demonstrate linking.  Pick up
 ;	a variable stored by another subroutine.
 ;
 ;	WB8RCR - 16-Nov-04
-;	$Revision: 1.1 $ $Date: 2004-11-16 11:24:42-05 $
+;	$Revision: 1.2 $ $Date: 2005-01-20 16:23:36-05 $
 ;===========================================================
 
 		global		Sub2
