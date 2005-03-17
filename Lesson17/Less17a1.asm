fc0fb6b1564992cc611d44ab39f048b659542e6e
John J. McDonough <jjmcd@fedoraproject.org>
2005-03-17 17:23:04 -0400

    Clean up some trash left from library

--- Lesson17/Less17a1.asm
+++ Lesson17/Less17a1.asm
@@ -1,7 +1,7 @@
 ;	Less17a1 - Replace send nybble code in library
 ;
 ;	JJMcD - 2005-03-17
-;	$Revision: 1.1 $ $Date: 2005-03-17 09:36:58-04 $
+;	$Revision: 1.2 $ $Date: 2005-03-17 17:23:04-04 $
 			include		p16f84a.inc
 
 	; Provided Routines
@@ -13,20 +13,19 @@ LCDRS		equ			H'40'		; LCD register select bit in PORTB
 
 			code
 ; ------------------------------------------------------------------------
+	; Send a command nybble to the LCD
+LCDsndI:
+			andlw		00fh		; only use low order 4 bits
+			goto		Sndit
+; ------------------------------------------------------------------------
 	; Send data to the LCD
 LCDsndD:
 			andlw		00fh		; only use low order 4 bits
 			iorlw		LCDRS		; Select register
-			goto		$+2			; Skip over LCDsndI
-
-; ------------------------------------------------------------------------
-	; Send a command nybble to the LCD
-LCDsndI:
-			andlw		00fh		; only use low order 4 bits
-									; FALL THROUGH to SendLCD
 
 ; ------------------------------------------------------------------------
 	; Actually move the data
+Sndit:
 			movwf		PORTB		; Send data to PORTB
 			bsf			PORTB,LCDEN	; turn on enable bit
 			nop
