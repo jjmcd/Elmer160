45c7310a34890999d039778e7b6def0aac62f2c5
John J. McDonough <jjmcd@fedoraproject.org>
2005-04-25 10:26:48 -0400

    Change 0xxh to H'xx' for consistency'

--- Lesson17/Less17b1.asm
+++ Lesson17/Less17b1.asm
@@ -1,7 +1,7 @@
 ;	Less17b1 - Replace send nybble code in library
 ;
 ;	JJMcD - 2005-03-17
-;	$Revision: 1.1 $ $Date: 2005-03-19 09:25:44-04 $
+;	$Revision: 1.2 $ $Date: 2005-04-25 10:26:48-04 $
 			include		p16f84a.inc
 
 	; Provided Routines
@@ -15,12 +15,12 @@ LCDRS		equ			H'40'		; LCD register select bit in PORTB
 ; ------------------------------------------------------------------------
 	; Send a command nybble to the LCD
 LCDsndI:
-			andlw		00fh		; only use low order 4 bits
+			andlw		H'0f'		; only use low order 4 bits
 			goto		Sndit
 ; ------------------------------------------------------------------------
 	; Send data to the LCD
 LCDsndD:
-			andlw		00fh		; only use low order 4 bits
+			andlw		H'0f'		; only use low order 4 bits
 			iorlw		LCDRS		; Select register
 
 ; ------------------------------------------------------------------------
