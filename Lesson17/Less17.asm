211d9626a4b8215e13859fc697d0d8334fe7ef8d
John J. McDonough <jjmcd@fedoraproject.org>
2005-06-10 13:52:04 -0400

    Update comments, make it print nice

--- Lesson17/Less17.asm
+++ Lesson17/Less17.asm
@@ -1,7 +1,25 @@
-;	Less17 - Test program for Lesson 17 on LCD's
+;	Less17 - Test program for Lesson 17 on LCDs
+;
+;	Program to exercise routines in the LCD library.  Program will
+;	first display a message on the LCD in the normal fashion.  This
+;	tests LCD initialization and proper incrementing of display 
+;	location of successive characters.  The program will delay, then
+;	clear the display.  Next, a message will be displayed in an
+;	unusual order, testing the ability to address the character
+;	locations.  After another delay, the display will then be
+;	cleared and a message scrolled across the display.  After yet
+;	another delay, the display will be cleared and the process
+;	repeated.
+;
+;	All the tests are performed on the first 8 characters of the
+;	display.  On older PIC-ELs this is the entire display.  However
+;	some PIC-ELs had a 16 character display.  This program does not
+;	address the right 8 characters of that display.
 ;
 ;	JJMcD - 14-May-05
-;	$Revision: 1.1 $ $Date: 2005-05-14 16:27:52-04 $
+;	$Revision: 1.3 $ $State: Exp $ $Date: 2005-06-10 13:52:04-04 $
+
+			list		b=4,c=156,n=58,x=Off
 			include		p16f84a.inc
 			__config	_XT_OSC & _WDT_OFF & _PWRTE_ON
 
