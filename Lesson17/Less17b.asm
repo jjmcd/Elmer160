86a697c70b6ae5b35a6e2fbea9227332b73a4981
John J. McDonough <jjmcd@fedoraproject.org>
2005-08-07 10:36:42 -0400

    No longer used for lesson

--- Lesson17/Less17b.asm
+++ Lesson17/Less17b.asm
@@ -1,26 +1,57 @@
-;	Less17b.asm - Mainline to test sending letters to LCD
+;	Less17 - Test program for Lesson 17 on LCD's
 ;
-;	$Revision: 1.1 $ $Date: 2005-03-19 09:25:44-04 $
+;	JJMcD - 14-May-05
+;	$Revision: 1.2 $ $Date: 2005-08-07 10:36:34-04 $
 			include		p16f84a.inc
-			__config	_XT_OSC & _PWRTE_ON & _WDT_OFF
-			extern		LCDinit,LCDletr
+			__config	_XT_OSC & _WDT_OFF & _PWRTE_ON
+
+			extern		LCDinit,LCDclear,LCDaddr,LCDletr
+			extern		Del128ms
+	
+PB1			equ			4
+PB2			equ			3
+PB3			equ			2
 
 STARTUP		code
 			goto		Start
+
 			code
 Start
-			call		LCDinit
+			errorlevel	-302		; Initialize PORTA to
+			banksel		TRISA		; be inputs (actually,
+			movlw		H'ff'		; somewhat redundant)
+			movwf		TRISA
+			banksel		PORTA
+			errorlevel	+302
+			call		LCDinit		; Initialize the LCD
+			call		LCDclear	; and clear it
+Loop
+			movlw		H'3'		; Position cursor to
+			call		LCDaddr		; PB1 location
+			movlw		"-"			; Default character
+			btfss		PORTA,PB1	; PB1 pressed?
+			movlw		"1"			; Yes, make it a 1
+			call		LCDletr		; Display the character
 
-			movlw		'E'
-			call		LCDletr
-			movlw		'l'
-			call		LCDletr
-			movlw		'm'
-			call		LCDletr
-			movlw		'e'
-			call		LCDletr
-			movlw		'r'
+			movlw		H'7'
+			call		LCDaddr
+			movlw		"-"
+			btfss		PORTA,PB2
+			movlw		"2"
 			call		LCDletr
 
-aa			goto		aa
+			movlw		H'43'
+			call		LCDaddr
+			movlw		"-"
+			btfss		PORTA,PB3
+			movlw		"3"
+			call		LCDletr
+
+			movlw		H'09'
+			call		LCDaddr
+
+			call		Del128ms
+
+			goto		Loop
+
 			end
