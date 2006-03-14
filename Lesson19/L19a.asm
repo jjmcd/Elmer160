d8e3091edd8e25f18c1c7d5edca0e67388a75d12
John J. McDonough <jjmcd@fedoraproject.org>
2006-03-14 15:49:20 -0400

    Use 16F873, longer delay

--- Lesson19/L19a.asm
+++ Lesson19/L19a.asm
@@ -12,18 +12,19 @@
 ;
 ;**
 ;	WB8RCR - 8-Feb-06
-;	$Revision: 1.1 $ $State: Exp $ $Date: 2006-02-08 20:01:08-05 $
+;	$Revision: 1.2 $ $State: Exp $ $Date: 2006-03-14 15:49:20-04 $
 
-			include		p16f876.inc
+			include		p16f873.inc
 
 			__config	_WDT_OFF&_PWRTE_ON&_BODEN_OFF&_LVP_OFF&_DEBUG_OFF
-			errorlevel	-302
 
 #define PORTCMASK B'11111011'		; TRIS mask for port C
+#define DELAY H'fc'					; Delay counter - higher=shorter
 
-			udata
+			udata_shr
 c1			res			1			; Counter for
 c2			res			1			; long delay
+c3			res			1			; Really long
 LEDstate	res			1			; Current LED state
 
 STARTUP		code
@@ -33,10 +34,12 @@ Start
 	; ------------------------------------------------------
 	; Set the LED pin to output
 	; ------------------------------------------------------
+			errorlevel	-302
 			banksel		TRISC
 			movlw		PORTCMASK
 			movwf		TRISC
 			banksel		PORTC
+			errorlevel	+302
 			clrf		LEDstate
 
 	; ------------------------------------------------------
@@ -56,14 +59,18 @@ SetLED		movf		LEDstate,W	; Pick up the LED state
 			goto		Loop		; Go do it again
 
 	; ------------------------------------------------------
-	; Routine to hang around a while (~500 ms @ 1.4 MHz)
+	; Routine to hang around a while (~2 s @ 1.4 MHz)
 	; ------------------------------------------------------
 Snore
+			movlw		DELAY
+			movwf		c3
 			clrf		c1
 			clrf		c2
 Snore1		incfsz		c1,F
 			goto		Snore1
 			incfsz		c2,F
+			goto		Snore1
+			incfsz		c3,F
 			goto		Snore1
 			return
 
