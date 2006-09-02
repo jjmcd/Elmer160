c1811af3df685e64faa803ccbc8bed7d21ce035c
John J. McDonough <jjmcd@fedoraproject.org>
2006-09-02 12:38:16 -0400

    User Processor.inc, udata_shr, TMR0IF, banksel

--- Lesson20/ISRa.asm
+++ Lesson20/ISRa.asm
@@ -2,7 +2,7 @@
 			subtitle	'Part of Lesson 20 on interrupts'
 			list		b=4,c=132,n=77,x=Off
 
-			include		p16f84a.inc
+			include		Processor.inc
 
 ;------------------------------------------------------------------------
 ;**
@@ -13,11 +13,15 @@
 ;
 ;**
 ;	WB8RCR - 19-May-06
-;	$Revision: 1.3 $ $State: Exp $ $Date: 2006-05-22 20:10:29-04 $
+;	$Revision: 1.4 $ $State: Exp $ $Date: 2006-09-02 12:38:16-04 $
 
 			extern		binary,dirty
 
+	IF PROC == 84
 			udata
+	ELSE
+			udata_shr
+	ENDIF
 w_temp		res			1				; Save area for W
 status_temp	res			1				; Save area for status
 
@@ -27,6 +31,10 @@ IRQSVC		code
 			swapf		STATUS,W		; And the STATUS
 			movwf		status_temp		;
 
+			banksel		binary			; Don't know bank settings on
+										; entry, restoring STATUS will
+										; restore original bank
+
 			; Bump up the two-byte value we will display
 			incf		binary+1,F		; Increment low byte
 			btfsc		STATUS,Z		; Overflow? (incf doesn't affect C)
@@ -35,7 +43,11 @@ IRQSVC		code
 
 			; Note that if we do not clear T0IF, we will be interrupted again
 			; as soon as we do the retfie, so we will get nothing else done.
+		IF (PROC == 819) || (PROC == 88)
+			bcf			INTCON,TMR0IF
+		ELSE
 			bcf			INTCON,T0IF		; Clear the old interrupt
+		ENDIF
 
 			swapf		status_temp,W	; Restore the status
 			movwf		STATUS			; register
