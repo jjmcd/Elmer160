6dc567c2542e22c177cea490147d978886526ef1
John J. McDonough <jjmcd@fedoraproject.org>
2006-11-07 08:36:15 -0500

    Use Processor.inc, add LEDflg, support EEPGD processors

--- Lesson20/SaveCntB.asm
+++ Lesson20/SaveCntB.asm
@@ -2,7 +2,8 @@
 			subtitle	'Part of Lesson 20 on interrupts'
 			list		b=4,c=132,n=77,x=Off
 
-			include		p16f84a.inc
+			include		Processor.inc
+			include		EEPROM.inc
 
 ;------------------------------------------------------------------------
 ;**
@@ -12,22 +13,32 @@
 ;
 ;**
 ;	WB8RCR - 23-May-06
-;	$Revision: 1.1 $ $State: Exp $ $Date: 2006-05-23 14:30:28-04 $
+;	$Revision: 1.2 $ $State: Exp $ $Date: 2006-11-07 08:36:15-05 $
 
 			global		SaveCnt
-			extern		binary
+			extern		binary,LEDflg
 
 MYLIB		code
 SaveCnt:
+			movlw		H'06'			; Upper LED on
+			movwf		PORTB
+			movwf		LEDflg
 			bcf			INTCON,GIE		; Turn off interrupts
 
-			movlw		H'10'			; Set address in EEPROM
+			errorlevel	-302
+			banksel		EEADR
+
+			movlw		SAVADR			; Set address in EEPROM
 			movwf		EEADR			; to be written
+			banksel		binary
 			movf		binary,W		; Set data to be written
+			banksel		EEDATA
 			movwf		EEDATA			;
 
-			errorlevel	-302
 			banksel		EECON1			; Bank 1
+		IFDEF EEPGD						; For processors that support
+			bcf			EECON1,EEPGD	; it, ensure EEPROM not FLASH
+		ENDIF
 			bsf			EECON1,WREN		; Enable write
 			movlw		H'55'			; This sequence is
 			movwf		EECON2			; required before the
@@ -38,13 +49,13 @@ Wait1		btfsc		EECON1,WR		; Wait for write complete
 			goto		Wait1
 
 		; Now next byte
-			errorlevel	+302
 			banksel		EEADR
 			incf		EEADR,F			; Next byte to be written
+			banksel		binary
 			movf		binary+1,W		; Set data to be written
+			banksel		EEDATA
 			movwf		EEDATA			;
 
-			errorlevel	-302
 			banksel		EECON1			; Bank 1
 			movlw		H'55'			; This sequence is
 			movwf		EECON2			; required before the
@@ -56,8 +67,12 @@ Wait2		btfsc		EECON1,WR		; Wait for write complete
 
 			bcf			EECON1,WREN		; Disable write
 			errorlevel	+302
-			banksel		EEADR			; Back to bank 0
+			banksel		0				; Back to bank 0
 			bsf			INTCON,GIE		; Turn interrupts back on
+
+			movlw		H'0e'			; All LEDs off
+			movwf		LEDflg
+			movwf		PORTB
 
 			return
 			end
